f215c66b9bcc5a9b1de8618f2ceebbfd89633031
osSQL <os.cloudsql@gmail.com>
2025-04-03 22:33:55 +0300

    1.2.1

---
+++ spawner/enemy_spawner.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+@export var skeleton_scene:PackedScene 
+
+
+func _on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+		
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var random_distance = randi_range(380, 500)
+	var spawn_pos = player.global_position + (random_direction * random_distance)
+	
+	var enemy = skeleton_scene.instantiate() as Node2D
+	get_parent().add_child(enemy)
+	
+	enemy.global_position = spawn_pos 
