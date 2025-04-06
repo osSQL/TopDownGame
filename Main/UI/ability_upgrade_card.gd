0876679d3ea9399743669f05cafc66aed622527b
osSQL <os.cloudsql@gmail.com>
2025-04-06 20:17:10 +0300

    1.4.3

--- Main/UI/ability_upgrade_card.gd
+++ Main/UI/ability_upgrade_card.gd
@@ -1,9 +1,16 @@
 extends PanelContainer
 class_name AbilityupgradeCard
 
+signal card_selected
+
 @onready var name_label = %NameLabel
 @onready var description_label = %DescriptionLabel
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+
+
+func _on_gui_input(event):
+	if event.is_action_pressed("left_click"):
+		card_selected.emit()
--- Main/manager/upgrade_manager.gd
+++ Main/manager/upgrade_manager.gd
@@ -17,6 +17,7 @@ func on_level_up(current_level):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([choosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 		
 	
 func apply_upgrade(upgrade: AbilityUpgrade):
@@ -31,4 +32,7 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
 	
+	print(current_upgrades)
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
 	
