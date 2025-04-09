a159c9f80836862ed90712bee2d1762db37f220e
osSQL <os.cloudsql@gmail.com>
2025-04-09 11:58:45 +0300

    1.6.1

--- Main/game_objects/player/player.gd
+++ Main/game_objects/player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var health_component = $health_component
 @onready var grace_period = $GracePeriod
 @onready var progress_bar = $ProgressBar
+@onready var ability_manager = $ability_manager
 
 
 
@@ -13,6 +14,7 @@ var enemies_colliding = 0
 func _ready():
 	health_component.died.connect(on_died)
 	health_component.health_changed.connect(on_health_changed)
+	Global.ability_upgrade_added.connect(on_ability_upgrade_added)
 	health_update()
 
 func _process(delta):
@@ -53,7 +55,13 @@ func on_died():
 func on_health_changed():
 	health_update()
 	
-
-
 func _on_grace_period_timeout():
 	check_if_damaged()
+	
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if not upgrade is NewAbility:
+		return
+		
+	var new_ability = upgrade as NewAbility
+	ability_manager.add_child(new_ability.new_ability_scene.instantiate())
+	
