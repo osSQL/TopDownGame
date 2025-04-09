25bf4d4eee0b005aa045a712c747eb692c2a09bb
osSQL <os.cloudsql@gmail.com>
2025-04-09 14:18:54 +0300

    1.6.4

--- Main/game_objects/skeleton/skeleton.gd
+++ Main/game_objects/skeleton/skeleton.gd
@@ -1,17 +1,17 @@
 extends CharacterBody2D
 
-var speed = 50
-
 @onready var health_component = $health_component
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var movement_component = $MovementComponent
 
+@export var death_scene: AnimatedSprite2D
 func _ready():
 	health_component.died.connect(on_died)
 
 func _process(delta):
-	var direction = get_direction_to_player()
-	velocity = speed * direction
-	move_and_slide()
+	var direction = movement_component.get_direction()
+	movement_component.move_to_player(self)
+	
 	if direction.x != 0 || direction.y != 0:
 		animated_sprite_2d.play("run")
 	else:
@@ -21,11 +21,8 @@ func _process(delta):
 	if face_sign != 0:
 		animated_sprite_2d.scale.x = face_sign
 
-func get_direction_to_player():
-	var player = get_tree().get_first_node_in_group("player") as Node2D
-	if player != null: 
-		return (player.global_position - global_position).normalized()
-	return Vector2.ZERO
 
-func on_died():
+
+func on_died():	
+	
 	queue_free()
