96d318f296360273ae7e5ec0c9aa0b630b5afd29
osSQL <os.cloudsql@gmail.com>
2025-04-10 16:21:40 +0300

    1.6.6

--- Main/manager/spawner/enemy_spawner.gd
+++ Main/manager/spawner/enemy_spawner.gd
@@ -4,12 +4,15 @@ extends Node
 
 @export var arena_time_manager: ArenaTimeManager
 @export var skeleton_scene:PackedScene 
+@export var vampire_scene:PackedScene 
 
 var base_spawn_time
 var min_spawn_time = 0.2
 var difficulty_multiplier = 0.01
+var enemy_pool = EnemyPool.new()
 
 func _ready():
+	enemy_pool.add_mob(skeleton_scene,30)
 	base_spawn_time = timer.wait_time
 	arena_time_manager.difficulty_increased.connect(on_difficulty_increased)
 	
@@ -35,7 +38,8 @@ func _on_timer_timeout():
 	if player == null:
 		return
 		
-	var enemy = skeleton_scene.instantiate() as Node2D
+	var choosen_mob = enemy_pool.pick_mob()
+	var enemy = choosen_mob.instantiate() as Node2D
 	var back_layer = get_tree().get_first_node_in_group("back_layer")
 	back_layer.add_child(enemy)
 	
@@ -44,4 +48,6 @@ func _on_timer_timeout():
 func on_difficulty_increased(difficulty_level:int):
 	var new_spawn_time = max(min_spawn_time, (base_spawn_time - (difficulty_level * difficulty_multiplier)))
 	timer.wait_time = new_spawn_time
-	print(timer.wait_time)
+	
+	if difficulty_level == 1:
+		enemy_pool.add_mob(vampire_scene, 70)
