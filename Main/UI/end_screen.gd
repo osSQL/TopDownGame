2bec4e341cadb8de1d69c9c1f944dbe721e1dfaf
osSQL <os.cloudsql@gmail.com>
2025-04-13 13:46:11 +0300

    1.8

--- Main/UI/end_screen.gd
+++ Main/UI/end_screen.gd
@@ -2,8 +2,14 @@ extends CanvasLayer
 class_name EndScreen
 
 @onready var name_label = %NameLabel
+@onready var panel_container = $MarginContainer/PanelContainer
 
 func _ready():
+	panel_container.pivot_offset = panel_container.size / 2
+	panel_container.scale = Vector2.ZERO
+	var tween = create_tween()
+	tween.tween_property(panel_container,"scale",Vector2.ZERO,0)
+	tween.tween_property(panel_container,"scale", Vector2.ONE, .3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	get_tree().paused = true
 	
 func change_to_victory():
